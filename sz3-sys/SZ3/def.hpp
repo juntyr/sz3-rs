91714e934914676a400cde7744939f3d438b9002
Juniper Tyree <juniper.tyree@helsinki.fi>
2023-10-08 21:55:55 +0000

    Stub alloc and free

--- sz3-sys/SZ3/def.hpp
+++ sz3-sys/SZ3/def.hpp
@@ -2,6 +2,8 @@
 #define _DEF_HPP
 
 #include <cmath>
+#include <cstdlib>
+#include <new>
 
 namespace SZ {
 
@@ -10,5 +12,39 @@ namespace SZ {
 
 }
 
+extern "C" void __cxa_pure_virtual() { abort(); }
+extern "C" void __cxa_allocate_exception() { abort(); }
+extern "C" void __cxa_throw() { abort(); }
+extern "C" unsigned int __cxa_uncaught_exceptions() { return 0; }
+
+void* operator new(std::size_t n)
+{
+  return std::malloc(n);
+}
+
+void* operator new[](std::size_t n)
+{
+    return std::malloc(n);
+}
+
+void* operator new(std::size_t n, std::align_val_t a)
+{
+    return std::aligned_alloc(static_cast<std::size_t>(a), n);
+}
+
+void operator delete(void* p) noexcept
+{
+    std::free(p);
+}
+
+void operator delete[](void* p) noexcept
+{
+    std::free(p);
+}
+
+void operator delete(void* p, std::align_val_t a) noexcept
+{
+    std::free(p);
+}
 
 #endif
